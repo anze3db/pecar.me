786f4d10508c3e31c3aa0cd684d0668121ae9d9d
Anže Pečar <anze@pecar.me>
2021-01-29 06:48:51 +0000

    Add tailwind

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ My personal home page.
 
 ```
 python -m http.server 1987
+npm run dev # Bloated css build
+npm run publish # Optimized css build
 ```
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./index.html'],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
